9c7c701af620a561be03b4fb99a4ed5ffe01339d
Nicholas Richards <nicholasrichards97@gmail.com>
2023-01-14 17:05:26 -0500

    updated testConfig struct to be more universal to other tests

--- chap1/manual-parse/parse_flags_test.go
+++ chap1/manual-parse/parse_flags_test.go
@@ -9,31 +9,31 @@ import (
 type testConfig struct {
 	args []string
 	err  error
-	config
+	c    config
 }
 
 // testing module identifies functions that start with Test?
 func TestParseArgs(t *testing.T) {
 	tests := []testConfig{
 		{
-			args:   []string{"-h"},
-			err:    nil,
-			config: config{printUsage: true, numTimes: 0}, // composite literals require , at the end?
+			args: []string{"-h"},
+			err:  nil,
+			c:    config{printUsage: true, numTimes: 0}, // composite literals require , at the end?
 		},
 		{
-			args:   []string{"10"},
-			err:    nil,
-			config: config{printUsage: false, numTimes: 10},
+			args: []string{"10"},
+			err:  nil,
+			c:    config{printUsage: false, numTimes: 10},
 		},
 		{
-			args:   []string{"abc"},
-			err:    errors.New("strconv.Atoi: parsing \"abc\": invalid syntax"),
-			config: config{printUsage: false, numTimes: 0},
+			args: []string{"abc"},
+			err:  errors.New("strconv.Atoi: parsing \"abc\": invalid syntax"),
+			c:    config{printUsage: false, numTimes: 0},
 		},
 		{
-			args:   []string{"1", "foo"},
-			err:    errors.New("Invalid number of arguments"),
-			config: config{printUsage: false, numTimes: 0},
+			args: []string{"1", "foo"},
+			err:  errors.New("Invalid number of arguments"),
+			c:    config{printUsage: false, numTimes: 0},
 		}, // composite literals require , at the end?
 	}
 
@@ -47,11 +47,11 @@ func TestParseArgs(t *testing.T) {
 		if tc.err == nil && err != nil {
 			t.Errorf("Expected nil error, got: %v\n", err)
 		}
-		if c.printUsage != tc.printUsage {
-			t.Errorf("Expected printUsage to be: %v, got: %v\n", tc.printUsage, c.printUsage)
+		if c.printUsage != tc.c.printUsage {
+			t.Errorf("Expected printUsage to be: %v, got: %v\n", tc.c.printUsage, c.printUsage)
 		}
-		if c.numTimes != tc.numTimes {
-			t.Errorf("Expected numTimes to be: %v, got: %v\n", tc.numTimes, c.numTimes)
+		if c.numTimes != tc.c.numTimes {
+			t.Errorf("Expected numTimes to be: %v, got: %v\n", tc.c.numTimes, c.numTimes)
 		}
 	}
 }
